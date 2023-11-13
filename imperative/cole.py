3b20d08143b7ee479ba61103e9c880294809f61f
Simon Cole <simonccole1@gmail.com>
2023-11-13 10:36:33 -0500

    Added manual test case data

---
+++ imperative/cole.py
@@ -0,0 +1,78 @@
+t1 = {
+    'val': 1,
+    'children': [
+        {
+            'val': 2,
+            'children': [
+                {
+                    'val': 5,
+                    'children': [
+                        {
+                            'val': 11,
+                            'children': []
+                        }
+                    ]
+                },
+                {
+                    'val': 6,
+                    'children': [
+                        {
+                            'val': 12,
+                            'children': [
+                                {
+                                    'val': 17,
+                                    'children': []
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            'val': 3,
+            'children': [
+                {
+                    'val': 7,
+                    'children': [
+                        {
+                            'val': 13,
+                            'children': []
+                        }
+                    ]
+                },
+                {
+                    'val': 8,
+                    'children': [
+                        {
+                            'val': 14,
+                            'children': []
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            'val': 4,
+            'children': [
+                {
+                    'val': 9,
+                    'children': []
+                },
+                {
+                    'val': 10,
+                    'children': [
+                        {
+                            'val': 15,
+                            'children': []
+                        },
+                        {
+                            'val': 16,
+                            'children': []
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
