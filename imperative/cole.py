7936850bdb59b421218301eb7df6530b8976fd5e
Simon Cole <simonccole1@gmail.com>
2023-11-14 11:17:04 -0500

    Implemented iterative DF and fixed data

--- imperative/cole.py
+++ imperative/cole.py
@@ -10,12 +10,7 @@ t1 = {
                         {
                             'val': 11,
                             'children': []
-                        }
-                    ]
-                },
-                {
-                    'val': 6,
-                    'children': [
+                        },
                         {
                             'val': 12,
                             'children': [
@@ -26,6 +21,12 @@ t1 = {
                             ]
                         }
                     ]
+                },
+                {
+                    'val': 6,
+                    'children': [
+                        
+                    ]
                 }
             ]
         },
@@ -34,21 +35,11 @@ t1 = {
             'children': [
                 {
                     'val': 7,
-                    'children': [
-                        {
-                            'val': 13,
-                            'children': []
-                        }
-                    ]
+                    'children': []
                 },
                 {
                     'val': 8,
-                    'children': [
-                        {
-                            'val': 14,
-                            'children': []
-                        }
-                    ]
+                    'children': []
                 }
             ]
         },
@@ -57,7 +48,16 @@ t1 = {
             'children': [
                 {
                     'val': 9,
-                    'children': []
+                    'children': [
+                        {
+                            'val': 13,
+                            'children': []
+                        },
+                        {
+                            'val': 14,
+                            'children': []
+                        }
+                    ]
                 },
                 {
                     'val': 10,
@@ -76,3 +76,30 @@ t1 = {
         }
     ]
 }
+inputTree = t1
+
+
+#visit when there are no unvisited children
+#iterate through children to see
+#once visited pop from stack
+#go to first unvisited child
+def DepthFirst(tree):
+    visited = set()
+    stack = [tree]
+    while(stack):
+        #add to the stack the first unvisited child
+        unVisited = False
+        stackTop = stack[-1]
+        for child in stackTop["children"]:
+            if child["val"] not in visited:
+                stack.append(child)
+                unVisited = True
+                break
+        #if there were no unvisited children, visit, pop
+        if not unVisited:
+            val = stack.pop()["val"]
+            print(val)
+            visited.add(val)
+
+
+DepthFirst(t1)
