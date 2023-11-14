71d8a4ee6ab0eb65764e44f9d23b4ae8bdd6c991
Simon Cole <simonccole1@gmail.com>
2023-11-14 11:29:52 -0500

    iterative BF

--- imperative/cole.py
+++ imperative/cole.py
@@ -84,6 +84,7 @@ inputTree = t1
 #once visited pop from stack
 #go to first unvisited child
 def DepthFirst(tree):
+    print("Traversing Depth First Post-Order")
     visited = set()
     stack = [tree]
     while(stack):
@@ -101,5 +102,20 @@ def DepthFirst(tree):
             print(val)
             visited.add(val)
 
+def BreadthFirst(tree):
+    print("Traversing Breadth First")
+    #When you get to node, visit
+    #add each child to a queue visit each and add each of its children to a queue
+    visited = set()
+    queue = [tree]
+    while queue:
+        #visit and remove
+        print(queue[0]["val"])
+        visited.add(queue[0]["val"])
+        for child in queue[0]["children"]: queue.append(child)
+        queue.pop(0)
 
-DepthFirst(t1)
+
+# DepthFirst(t1)
+BreadthFirst(t1)
+
